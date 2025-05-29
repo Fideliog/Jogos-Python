19ee5d2516e5c174b6079d15ad5ce3b4841b4f2a
Fideliog <eduardoledo36@gmail.com>
2025-05-29 00:51:44 -0300

    PPP

--- TacoMaker/TacoMaker.py
+++ TacoMaker/TacoMaker.py
@@ -22,12 +22,22 @@ Ingre_trasform = pygame.image.load(os.path.join(diretorio_imagens,'Ingredientes.
 
 pygame.init()
 
+tempo_inicial = pygame.time.get_ticks()
+intervalo_subida = 1000
+caveira_extra_adicionada = False
 largura = 900
 altura = 700
 janela = pygame.display.set_mode([largura,altura])
+sprite_sheet_coracao = pygame.image.load(os.path.join(diretorio_imagens,'CoracaoBril.png')).convert_alpha()
 delay = 0
 IngredientesRec = 0
 Vidas = 3
+vel_alface = 2.0
+vel_tomate = 2.0
+vel_carne = 2.0
+vel_queijo = 2.0
+vel_caveira = 4.0
+vel_maxima = 15.0
 pygame.display.set_caption('TacoMaker')
 relogio = pygame.time.Clock()
 RodarJogo = True
@@ -43,12 +53,21 @@ def VoltarMain():
     RodarMainPage = True
 
 def ReiniciarJogo():
-    global RodarMainPage, protax, protay, RodarFase, IngredientesRec
+    global RodarMainPage, protax, protay, RodarFase, IngredientesRec, Vidas, delay, vel_alface, vel_tomate, vel_carne, vel_queijo, vel_caveira
     RodarMainPage = False
     RodarFase = True
     protax = 70
     protay = 450
+    Vidas = 3
     IngredientesRec = 0
+    delay = 1
+    vel_alface = 2.0
+    vel_tomate = 2.0
+    vel_carne = 2.0
+    vel_queijo = 2.0
+    vel_caveira = 4.0
+    Sprite_coracao.add(corasao3)
+    Sprite_coracao.add(corasao2)
 
 class Ingredientes(pygame.sprite.Sprite):
 
@@ -65,18 +84,20 @@ class Ingredientes(pygame.sprite.Sprite):
             self.Sprites_Ingre.append(img)
 
         self.image = self.Sprites_Ingre[self.Tipo]
-        self.image = pygame.transform.scale(self.image, (16*4, 14*4))
+        self.image = pygame.transform.scale(self.image, (16*5, 14*5))
         self.rect = self.image.get_rect()
         self.rect.topleft = randint(80, 660), 40
 
     def update(self):
+        global IngredientesRec
         self.image = self.Sprites_Ingre[int(self.Tipo)]
-        self.image = pygame.transform.scale(self.image, (16*4, 14*4))
+        self.image = pygame.transform.scale(self.image, (16*5, 14*5))
         self.rect.y = self.rect.y + self.velocidade
 
         if (self.rect.bottomleft[1] >= 560 ):
             self.rect.x = randint(80, 660)
             self.rect.y = 40
+            IngredientesRec = IngredientesRec - 1
 
 class TacoProta(pygame.sprite.Sprite):
 
@@ -87,16 +108,24 @@ class TacoProta(pygame.sprite.Sprite):
 
         self.atual = 0
         self.image = self.TacoSprite[self.atual]
-        self.image = pygame.transform.scale(self.image, (44*3.5, 29*3.5))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = 390, 450
+        self.image = pygame.transform.scale(self.image, (44*3.75, 29*3.75))
+        visual_rect = self.image.get_rect()
+        visual_center = visual_rect.center
 
-    def update(self):
+        hitbox_width = int(visual_rect.width * 0.80)
+        hitbox_height = int(visual_rect.height * 0.80)
+
+        self.rect = pygame.Rect(0, 0, hitbox_width, hitbox_height)
+        self.rect.center = visual_center
+        self.rect.topleft = 390, 480
+
+    def update(self):        
         self.atual = self.atual + 0.20
         if self.atual >= len(self.TacoSprite):
             self.atual = 0
         self.image = self.TacoSprite[int(self.atual)]
-        self.image = pygame.transform.scale(self.image, (44*3.5, 29*3.5))
+        self.image = pygame.transform.scale(self.image, (44*3.75, 29*3.75))
+
 
         if pygame.key.get_pressed()[K_a]:
             if (self.rect.x >= 90 ):
@@ -105,6 +134,30 @@ class TacoProta(pygame.sprite.Sprite):
             if (self.rect.x < 650):
                 self.rect.x = self.rect.x + 20
 
+class Coracao(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.Sprite_coracao = []
+
+        for i in range(5):
+            img = sprite_sheet_coracao.subsurface((i * 14,0),(14,13))
+            self.Sprite_coracao.append(img)
+
+        self.sprite_atual = 0
+        self.image = self.Sprite_coracao[self.sprite_atual]
+        self.image = pygame.transform.scale(self.image, (14*2.5, 13*2.5))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = 700, 597
+
+    def update(self):
+        self.sprite_atual = self.sprite_atual + 0.20
+        if self.sprite_atual >= len(self.Sprite_coracao):
+            self.sprite_atual = 0
+
+        self.image = self.Sprite_coracao[int(self.sprite_atual)]
+        self.image = pygame.transform.scale(self.image, (14*2.5, 13*2.5))
 
 class MainPage(pygame.sprite.Sprite):
 
@@ -138,15 +191,27 @@ Sprites_Car = pygame.sprite.Group()
 Sprites_To = pygame.sprite.Group()
 Sprites_Que = pygame.sprite.Group()
 Sprites_Morte = pygame.sprite.Group()
+Sprite_coracao = pygame.sprite.Group()
 
-Alface = Ingredientes(0,5)
-Carne = Ingredientes(1,5)
-Tomate = Ingredientes(2,5)
-Queijo = Ingredientes(3,5)
-Caveira = Ingredientes(4,10)
+Alface = Ingredientes(0, vel_alface)
+Carne = Ingredientes(1, vel_carne)
+Tomate = Ingredientes(2, vel_tomate)
+Queijo = Ingredientes(3, vel_queijo)
+Caveira = Ingredientes(4, vel_caveira)
+
 
 TacoBalde = TacoProta()
 MainPageTaco = MainPage()
+corasao1 = Coracao()
+corasao2 = Coracao()
+corasao3 = Coracao()
+
+corasao2.rect.topleft = 740, 597
+corasao3.rect.topleft = 780, 597
+
+Sprite_coracao.add(corasao1)
+Sprite_coracao.add(corasao2)
+Sprite_coracao.add(corasao3)
 
 Sprites_Al.add(Alface)
 Sprites_To.add(Tomate)
@@ -176,42 +241,68 @@ while RodarJogo:
 
         pygame.display.flip()
 
-
     while RodarFase:
 
         relogio.tick(30)        
         janela.blit(TelaGame, (0,0))
         Sprites_Geral.draw(janela)
         Sprites_Geral.update()
-        
+
+        if Vidas < 3:
+            Sprite_coracao.remove(corasao3)
+        if Vidas < 2:
+            Sprite_coracao.remove(corasao2)
+            
+        Sprite_coracao.draw(janela)
+        Sprite_coracao.update()
+
         Sprites_Al.draw(janela)
         Sprites_Al.update()
 
-        if delay > 30:
+        if delay > 60:
             Sprites_To.draw(janela)
             Sprites_To.update()
 
-        if delay > 60:
+        if delay > 120:
             Sprites_Car.draw(janela)
             Sprites_Car.update()
 
-        if delay > 90:
+        if delay > 180:
             Sprites_Que.draw(janela)
             Sprites_Que.update()
         
-        if delay > 120:
+        if delay > 240:
             Sprites_Morte.draw(janela)
             Sprites_Morte.update()
+        if vel_tomate >= vel_maxima and not caveira_extra_adicionada:
+            nova_caveira = Ingredientes(4, vel_caveira)
+            Sprites_Morte.add(nova_caveira)
+            caveira_extra_adicionada = True
 
         delay = delay + 1
-        velocidade = velocidade + 0.25
-        if velocidade == 5:
-            Alface.velocidade = Alface.velocidade + 1
-            Tomate.velocidade = Tomate.velocidade + 1
-            Carne.velocidade =  Carne.velocidade + 1
-            Queijo.velocidade = Queijo.velocidade + 1
-            Caveira.velocidade =  Caveira.velocidade + 1
-            velocidade = 1
+
+        tempo_atual = pygame.time.get_ticks()
+        if tempo_atual - tempo_inicial > intervalo_subida:
+            incremento = 0.2
+
+            if vel_alface < vel_maxima:
+                vel_alface += incremento
+                Alface.velocidade = vel_alface
+            if vel_tomate < vel_maxima:
+                vel_tomate += incremento
+                Tomate.velocidade = vel_tomate
+            if vel_carne < vel_maxima:
+                vel_carne += incremento
+                Carne.velocidade = vel_carne
+            if vel_queijo < vel_maxima:
+                vel_queijo += incremento
+                Queijo.velocidade = vel_queijo
+            if vel_caveira < vel_maxima + 2:
+                vel_caveira += incremento
+                Caveira.velocidade = vel_caveira
+            tempo_inicial = tempo_atual
+
+        print(f"Velocidade aumentada: {Alface.velocidade}")
 
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
@@ -228,13 +319,18 @@ while RodarJogo:
 
         for grupo, tipo in ingredientes_info:
             if pygame.sprite.spritecollide(TacoBalde, grupo, True):
-                vel = 5 + int(velocidade / 20)
-                grupo.add(Ingredientes(tipo, vel))
-                IngredientesRec += 1
+                if tipo == 0:
+                    grupo.add(Ingredientes(tipo, vel_alface))
+                elif tipo == 1:
+                    grupo.add(Ingredientes(tipo, vel_carne))
+                elif tipo == 2:
+                    grupo.add(Ingredientes(tipo, vel_tomate))
+                elif tipo == 3:
+                    grupo.add(Ingredientes(tipo, vel_queijo))
+                IngredientesRec += 3
 
         if pygame.sprite.spritecollide(TacoBalde, Sprites_Morte, True):
-            vel = 10 + int(velocidade / 20)
-            Sprites_Morte.add(Ingredientes(4, vel))
+            Sprites_Morte.add(Ingredientes(4, vel_caveira))
             Vidas -= 1
 
         if Vidas == 0:
@@ -253,6 +349,7 @@ while RodarJogo:
                     if event.type == KEYDOWN:
                         if event.key == K_r:
                             ReiniciarJogo()
+                            perdeu = False
                         if event.key == K_ESCAPE:
                             VoltarMain()
                             perdeu = False
